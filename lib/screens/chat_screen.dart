5cd3823218174234386ca8b954e3581e34e394e3
Manuel Kpatsu <manuelkpatsu@gmail.com>
2021-06-23 08:18:36 +0000

    Wrap up authentication and logout

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,11 +1,41 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('The Messenger'),
+        actions: [
+          DropdownButton(
+            icon: Icon(
+              Icons.more_vert,
+              color: Theme.of(context).primaryIconTheme.color,
+            ),
+            items: [
+              DropdownMenuItem(
+                child: Container(
+                  child: Row(
+                    children: [
+                      Icon(Icons.exit_to_app),
+                      SizedBox(width: 8),
+                      Text('Logout'),
+                    ],
+                  ),
+                ),
+                value: 'logout',
+              ),
+            ],
+            onChanged: (itemIdentifier) {
+              if (itemIdentifier == 'logout') {
+                FirebaseAuth.instance.signOut();
+              }
+            },
+          )
+        ],
+      ),
       body: StreamBuilder(
         stream: FirebaseFirestore.instance
             .collection('chats/8xfKRFMOuyiywwSLqS9J/messages')
@@ -31,10 +61,9 @@ class ChatScreen extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
-          FirebaseFirestore.instance.collection('chats/8xfKRFMOuyiywwSLqS9J/messages')
-            .add({
-              'text': 'This was added by clicking the button'
-            });
+          FirebaseFirestore.instance
+              .collection('chats/8xfKRFMOuyiywwSLqS9J/messages')
+              .add({'text': 'This was added by clicking the button'});
         },
       ),
     );
