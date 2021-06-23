4a5c24b53a33a1bd79811199e77eea2b44a7bd76
Manuel Kpatsu <manuelkpatsu@gmail.com>
2021-06-23 16:33:26 +0000

    Display user images

--- lib/widgets/chat/messages.dart
+++ lib/widgets/chat/messages.dart
@@ -30,6 +30,7 @@ class Messages extends StatelessWidget {
             return MessageBubble(
               chatDocs[index]['text'],
               chatDocs[index]['username'],
+              chatDocs[index]['userImage'],
               chatDocs[index]['userId'] == currentUser.uid,
               key: ValueKey(chatDocs[index].id),
             );
